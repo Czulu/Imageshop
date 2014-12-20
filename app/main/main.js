39063c6f5a8e7bb67e2ad3951c67c4925231404c
Czulu <pazdalskiandrzej@gmail.com>
2014-12-20 20:46:57 +0100

    added responsive grid for search results

--- app/main/main.js
+++ app/main/main.js
@@ -1,24 +1,35 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name imageshopApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the imageshopApp
- */
 angular.module('imageshopApp')
-  .controller('MainCtrl', function ($scope, API, $http) {
-      $scope.networkError = false;
+  .controller('MainCtrl', function ($scope, API, $rootScope) {
 
+      /*
+      using rootscope to prevent wiping search results by view change
+      */
+      $rootScope.searchResults = $rootScope.searchResults || {};
+      $rootScope.searchResults.data = $rootScope.searchResults.data || null;
+      $rootScope.searchResults.currentPage = $rootScope.searchResults.currentPage || 1;
+      $scope.networkError = false;
       $scope.cartItems = 0;
+
       $scope.findImages = function (query) {
           API.newSearch(query).success(function (data) {
+              var results = [],
+                  leng = data.query.allimages.length;
+
               $scope.networkError = false;
-              $scope.searchResults = data.query.allimages;
+              /*
+              filtering videos from data returned by API
+              */
+              for (var i = 0; i < leng; i++) {
+                  if (data.query.allimages[i].mediatype === 'BITMAP') {
+                      results.push(data.query.allimages[i]);
+                  }
+              }
+              $scope.searchResults.data = results;
+              $scope.searchResults.currentPage = 1;
           }).error(function (data) {
               $scope.networkError = true;
-              $scope.searchResults = null;
           });
       }
   });
---
+++ app/scripts/directives/image-grid.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+
+angular.module('imageshopApp').directive('imageGrid', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+
+            function applyGrid() {
+                /*
+                Adding small delay to make sure that nodes are rendered before
+                stylying is applied
+                */
+                setTimeout(function () {
+                    var options = {
+                        minMargin: 20,
+                        maxMargin: 20,
+                        itemSelector: attrs.imageGrid, //images container selector
+                        firstItemClass: "first-item"
+                    };
+                    jQuery(element).rowGrid(options);
+                }, 1000);
+            }
+
+            applyGrid();
+
+            scope.$watch(function (scope) {
+                return scope.searchResults;
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    applyGrid();
+                }
+            }, true);
+        }
+    }
+});
