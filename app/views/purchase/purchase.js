309a360b1c377c18230761d965006ceea8ab7d9c
Czulu <pazdalskiandrzej@gmail.com>
2014-12-24 21:26:41 +0100

    added order form, automatic redirect to main page from purchase success page

--- app/views/purchase/purchase.js
+++ app/views/purchase/purchase.js
@@ -1,4 +1,15 @@
 ﻿'use strict';
 
-angular.module('imageshopApp').controller('PurchaseCtrl', function ($scope, ShoppingCart) {
+angular.module('imageshopApp').controller('PurchaseCtrl', function ($scope, $location, $timeout) {
+    $scope.redirectionTimer = 11;
+
+    (function redirectCountdown() {
+        $scope.redirectionTimer--;
+        if ($scope.redirectionTimer === 0) {
+            $location.path('/');
+            $scope.$apply();
+        } else {
+            $timeout(redirectCountdown, 1000);
+        }
+    })();
 });
